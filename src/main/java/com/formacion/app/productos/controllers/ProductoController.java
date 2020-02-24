047bc43a278ac0e4a1dfd4bde480697a3407c057
Ricardo Rodríguez <rrodrigueg.seiza@bancochile.cl>
2020-02-23 21:10:13 -0300

    Obteniendo puerto de aplicacion del application.properties

--- src/main/java/com/formacion/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacion/app/productos/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package com.formacion.app.productos.controllers;
 import com.formacion.app.productos.models.entity.Producto;
 import com.formacion.app.productos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,17 +22,22 @@ public class ProductoController {
     @Autowired
     private Environment environment;
 
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("/listar")
     public List<Producto> listar(){
         return productoService.findAll().stream().map(producto -> {
-            producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+            //producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+            producto.setPort(port);
             return producto;
         }).collect(Collectors.toList());
     }
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id){
         Producto producto = productoService.findById(id);
-        producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        //producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        producto.setPort(port);
         return producto;
     }
 }
