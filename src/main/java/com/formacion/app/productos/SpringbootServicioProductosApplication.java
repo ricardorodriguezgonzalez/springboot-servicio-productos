cd4770174cb2366411017bf77a586b3831c8bc6d
Ricardo Rodríguez <rrodrigueg.seiza@bancochile.cl>
2020-03-06 09:33:04 -0300

    Con el server Eureka configurado

--- src/main/java/com/formacion/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacion/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,8 @@ package com.formacion.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
